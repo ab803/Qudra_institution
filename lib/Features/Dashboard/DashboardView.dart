aa5608e48fd8c37ab66b40064a229ca96006e40b
Abdullah Adel <a.a.abokhalil5@gmail.com>
2026-04-24 22:08:11 +0300

    edit

--- lib/Features/Dashboard/DashboardView.dart
+++ lib/Features/Dashboard/DashboardView.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:qudra_institution/Features/Dashboard/repo/DashboardRepository.dart';
 import 'package:qudra_institution/Features/Dashboard/viewModel/dashboard_cubit.dart';
